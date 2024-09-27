3d8415d4e4c7c6ff451b9634e91dfb795eddf56e
alwin557 <jomiskids@yahoo.com>
2024-09-27 21:09:35 +0700

    commit pertama saya

---
+++ percobaan.js
@@ -0,0 +1,18 @@
+// memanggil modul bawaan dari node.js yaitu
+// untuk membuat server http
+
+const http = require('http')
+
+http.createServer( function (request, response) {
+    response.writeHead(200, {'content-type': 'text/html'})
+    response.end(
+        '<h1>halo guys, script ini di generate dari backend node.js, aji</h1><hr>'
+        
+    )    
+
+}).listen(3000, function () {
+    console.log('server sudah nyala, buka http://localhost:3000')
+})
+
+// kalau mau keluar dari server di terminal tekan control C
+// kalau mau masuk ke server di terminal perintah: node percobaan
